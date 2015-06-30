d5f63a39bcdade4606ce0f4c87e47e24404f1ac0
Arthur Savage <alerc@meta.ua>
2015-06-30 10:16:00 +0300

    Net Lib Tab

--- Tablenet.js
+++ Tablenet.js
@@ -1,7 +1,7 @@
  /*
-    <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
-		<script src="http://datatables.net/release-datatables/extensions/TableTools/js/dataTables.tableTools.js"></script>
+   <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
+   <script src="http://datatables.net/release-datatables/extensions/TableTools/js/dataTables.tableTools.js"></script>
  */
  
   $(document).ready(function(){$('#aplanner').DataTable();});
