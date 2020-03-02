174f7a8173504852710f4c6bc35c439faee409a5
Arthur Savage <alerc@meta.ua>
2020-03-02 09:05:28 +0200

    Create localstorage.js

---
+++ localstorage/localstorage.js
@@ -0,0 +1,9 @@
+ar testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+// Put the object into storage
+localStorage.setItem('testObject', JSON.stringify(testObject));
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('testObject');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
