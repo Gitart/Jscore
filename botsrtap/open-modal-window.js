0ffedbb125703320c0db9a765a46d3b41eb1415c
Arthur Savage <alerc@meta.ua>
2022-04-06 17:14:38 +0300

    Create open-modal-window.js

---
+++ botsrtap/open-modal-window.js
@@ -0,0 +1,22 @@
+var galleryModal = new bootstrap.Modal(document.getElementById('galleryModal'), {
+  keyboard: false
+});
+
+
+galleryModal.show();
+
+
+## Sample
+```js
+  // New document
+   function Openwindow() {
+       // id = Math.floor(new Date().valueOf() * Math.random());
+       id = new Date().valueOf();
+       $("#numdocument").html("Накладна # " + id);
+       $("#num").val(id);
+       // $("#createat").val(dt);
+       
+       var myModal = new bootstrap.Modal(document.getElementById("Mmodalwindow"), {});
+       myModal.show();
+   }
+   ```
