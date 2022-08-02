004aac731f963d3865fc6a350c5d94d2679cebb2
Arthur Savage <alerc@meta.ua>
2022-08-02 19:02:39 +0300

    Create binding.js

---
+++ binding.js
@@ -0,0 +1,20 @@
+$(window).load(function(){....}});
+
+$("#book").load(function(){....}});
+
+
+$("img.useicon").load(function(){
+if ($(this).height()>100{
+    $(this).addClass("big");
+}
+});
+
+
+$("#target").click(function(){
+  $("#other").click();
+}});
+
+
+$("#target").click(function(){
+  $(this).slideUp();
+}});
